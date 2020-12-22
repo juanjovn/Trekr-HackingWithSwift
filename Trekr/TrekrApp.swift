8bc8e066020a8cc3357eeb3e467ee8670f181dc7
Juanjo Valiño <juanjovn@gmail.com>
2020-12-22 18:20:42 +0100

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Juanjo Valiño on 22/12/20.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
